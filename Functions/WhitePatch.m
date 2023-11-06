588481a80daf4187f754fb3b198ba652d5b72adb
Erik Dahlström <erida600@student.liu.se>
2023-11-06 13:11:37 +0100

    Gray world & White Patch
    Gray world and White Patch tested and works

--- Functions/WhitePatch.m
+++ Functions/WhitePatch.m
@@ -2,7 +2,7 @@ function [outputImg] = WhitePatch(img)
 A_lin = rgb2lin(img);
 
 topPercentile = 5;
-illuminant = illumwhite(A,topPercentile);
+illuminant = illumwhite(A_lin,topPercentile);
 
 B_lin = chromadapt(A_lin,illuminant,"ColorSpace","linear-rgb");
 outputImg = lin2rgb(B_lin);
