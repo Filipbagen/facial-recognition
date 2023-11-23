efc595b0445a65ff7f3d97eef6bce760207b2d91
Emma Zettervall <emmze999@student.liu.se>
2023-11-23 15:27:23 +0100

    updaterad

--- Functions/Remake_scale.m
+++ Functions/Remake_scale.m
@@ -3,15 +3,18 @@ function [zoomedImage] = Remake_scale(img)
  % Get the size of the image
     [rows, cols, ~] = size(img);
 
-    % Specify the scale range
-    scaleRange = [0.9, 1.1];
-
     % Calculate the center of the image
     centerX = floor(cols / 2);
     centerY = floor(rows / 2);
 
+    min=0.5;
+    max=1.5;
+
+    % Generate a random tone change factor
+    scalingFactor = rand() * (max - min) + min;
+
     % Generate a random scaling factor within the specified range
-    scalingFactor = randi([scaleRange(1), scaleRange(2)]);
+    %scalingFactor = randi([scaleRange(1), scaleRange(2)]);
 
     % Calculate the cropping box
     cropBox = [centerX - floor(cols * scalingFactor / 2), ...
@@ -21,6 +24,8 @@ function [zoomedImage] = Remake_scale(img)
 
     % Crop and resize the image
     zoomedImage = imcrop(imresize(img, scalingFactor), cropBox);
+   
+
 
     % Display the original and zoomed-in images (optional)
     figure;
--- Functions/Remake_tone.m
+++ Functions/Remake_tone.m
@@ -1,26 +1,22 @@
 function [adjustedImage] = Remake_tone(originalImage)
-% Define the range for the random tone change (-30% to 30%)
-minToneChange = -30;
-maxToneChange = 30;
+    % Define the range for the random tone change (-30% to 30%)
+    minToneChange = -30;
+    maxToneChange = 30;
 
-% Generate a random tone change factor
-toneChangePercentage = rand * (maxToneChange - minToneChange) + minToneChange;
+    % Generate a random tone change factor
+    toneChangePercentage = rand() * (maxToneChange - minToneChange) + minToneChange;
 
-% Calculate the adjustment factors
-contrastFactor = 1 + toneChangePercentage / 100;  % Increase contrast
-brightnessFactor = toneChangePercentage / 100;    % Increase brightness
+    % Calculate the adjustment factor for brightness
+    brightnessFactor = toneChangePercentage ;    % Increase brightness
 
-% Scale the factors to be in the range [0.0, 1.0]
-contrastFactor = max(0, min(1, contrastFactor));
-brightnessFactor = max(0, min(1, brightnessFactor));
+    % Scale the factor to be in the range [0.0, 1.0]
+    %brightnessFactor = max(0, min(1, brightnessFactor));
 
-% Adjust the tone of the image
-adjustedImage = imadjust(originalImage, [brightnessFactor, contrastFactor], []);
+    % Adjust the tone of the image (only brightness)
+    adjustedImage = imadd(originalImage, brightnessFactor);
 
-% Display the original and adjusted images
-figure;
-subplot(1, 2, 1), imshow(originalImage), title('Original Image');
-subplot(1, 2, 2), imshow(adjustedImage), title('Adjusted Image');
-
+    % Display the original and adjusted images
+    figure;
+    subplot(1, 2, 1), imshow(originalImage), title('Original Image');
+    subplot(1, 2, 2), imshow(adjustedImage), title(['Adjusted Image (' num2str(brightnessFactor) ')%']);
 end
-
