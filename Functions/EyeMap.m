f8684ad664b4c7abcb97f2f55876b0c184c0b612
Erik Dahlström <erida600@student.liu.se>
2023-11-24 14:15:44 +0100

    Version 1
    Version1

--- Functions/EyeMap.m
+++ Functions/EyeMap.m
@@ -1,4 +1,4 @@
-function [eyeMap] = EyeMap(img)
+function [binaryEyeMap, circles, randii] = EyeMap(img)
     % Convert the image to YCbCr color space
     YCbCr_image = rgb2ycbcr(img);
     
@@ -12,10 +12,6 @@ function [eyeMap] = EyeMap(img)
     EyeMapC = (1/3) * ( (Cb.^2) + (Cr_neg.^2) + (Cb./Cr) );
     EyeMapC = EyeMapC./max(EyeMapC(:));
     
-    % Display EyeMapC
-    %imshow(EyeMapC);
-    %title('EyeMapC');
-    
     % Morphological operations on the luminance component (optional)
     se = strel('disk', 5); % Adjust the size of the structuring element
     y_dilate = imdilate(y, se);
@@ -25,14 +21,24 @@ function [eyeMap] = EyeMap(img)
     EyeMapL = y_dilate./(y_erode + 1);
     EyeMapL = EyeMapL./max(EyeMapL(:));
     
-%     Display EyeMapL
-%     imshow(EyeMapL);
-%     title('EyeMapL');
-    
-
     % Combine the chrominance and luminance eye maps
     output = EyeMapC .* EyeMapL;
+   
+    % Use Otsu's method to automatically determine the threshold
+    threshold = graythresh(output);
 
-    % Dilate the final eye map for better visualization
-    eyeMap = imdilate(output, se);
+    % Apply the threshold to convert to a binary image
+    binaryEyeMap = imbinarize(output, threshold);
+
+    % Remove small objects 
+    binaryEyeMap = bwareaopen(binaryEyeMap, 80);
+
+    % Dilate the final binary eye map for better visualization
+    binaryEyeMap = imdilate(binaryEyeMap, se);
+
+    % Apply circular Hough transform to find circles
+    radiusRange = [10, 55]; % Adjust the radius range based on expected eye size
+    sensitivity = 0.75; % Adjust the sensitivity based on your images
+    [circles, randii] = imfindcircles(binaryEyeMap, radiusRange, 'Sensitivity', sensitivity);
+    
 end
--- Functions/FaceMask.m
+++ Functions/FaceMask.m
@@ -1,48 +1,25 @@
-function faceMask = FaceMask(img)
+function binary_img = FaceMask(img)
     % Method 1: YCbCr color space
     YCbCr_image = rgb2ycbcr(img);
+    Y = YCbCr_image(:,:,1);
     Cb = YCbCr_image(:,:,2);
     Cr = YCbCr_image(:,:,3);
 
-    % Define thresholds for skin color segmentation in YCbCr space
-    Cb_min = 77;
-    Cb_max = 127;
-    Cr_min = 133;
-    Cr_max = 173;
+    % Adjusted thresholds for skin color segmentation in YCbCr space
+    Cb_min = 78;
+    Cb_max = 130;
+    Cr_min = 136;
+    Cr_max = 168;
 
-    % Create a binary mask based on the defined skin color thresholds
-    skinMask = (Cb >= Cb_min & Cb <= Cb_max) & (Cr >= Cr_min & Cr <= Cr_max);
 
-    % Method 2: HSV color space
-    img_hsv = rgb2hsv(img);
-    H = img_hsv(:,:,1);
-    S = img_hsv(:,:,2);
-    V = img_hsv(:,:,3);
+    % Threshold segmentation algorithm for skin
+    skin_mask = (Cb >= Cb_min & Cb <= Cb_max) & (Cr >= Cr_min & Cr <= Cr_max) & Y >= 80 ;
 
-    % Define thresholds for HSV space
-    Hmin = 0;
-    Hmax = 0.1; % Adjust according to the specific hue range for skin tones
-    Smin = 0.15; % Adjust according to the specific saturation range for skin tones
-    Vmin = 0.45; % Adjust according to the specific value/brightness range for skin tones
+    % Set the skin to white and the rest to black
+    binary_img = uint8(skin_mask) * 255;
 
-    % Create binary mask for HSV method
-    mask_hsv = (H >= Hmin & H <= Hmax & S >= Smin & V >= Vmin);
-
-    % Combine the masks
-    combined_mask = skinMask & mask_hsv;
-
-    % Morphological operations (optional)
-    se = strel('disk', 90); % Adjust the size of the structuring element
-    combined_mask = imclose(combined_mask, se);
-
-    % Remove small connected components (noise)
-    combined_mask = bwareaopen(combined_mask, 500);
-
-    % Convert the logical mask to uint8
-    combined_mask = uint8(combined_mask);
-
-    % Apply the combined mask to the original image
-    result_image = img .* repmat(combined_mask, [1, 1, 3]);
-
-    faceMask = result_image;
+    % Fill holes in the binary image
+    se = strel('square', 3);
+    binary_img = imfill(binary_img, 'holes');
+    binary_img = imclose(binary_img, se);
 end
