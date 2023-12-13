0824c76ba99c253a14b51b991bf89a223fd07615
Emma Zettervall <emmze999@student.liu.se>
2023-12-13 10:44:30 +0100

    Update tone_compensation.m

--- Functions/tone_compensation.m
+++ Functions/tone_compensation.m
@@ -1,32 +1,40 @@
 function [result] = tone_compensation(originalRGB)
-    % gray_image = rgb2gray(image)
-    % [counts, intensities] = imhist(gray_image, 2^16)
-    % cumulative_counts = cumsum(counts)
-    % index = find(cumulative_counts >= numel(gray_image) * 0.5, 1, 'first')
-    % intensity = intensities(index)
-    % %if(intensity < 0.5); intensity = 0.5; end
-    % result = immultiply(image, 0.5 / intensity)
 
-    % % Perform intensity compensation for each color channel
-    % compensatedR = histeq(originalRGB(:,:,1));
-    % compensatedG = histeq(originalRGB(:,:,2));
-    % compensatedB = histeq(originalRGB(:,:,3));
+    % % Separate the image into three channels (R, G, B)
+    % redChannel = originalRGB(:, :, 1);
+    % greenChannel = originalRGB(:, :, 2);
+    % blueChannel = originalRGB(:, :, 3);
     % 
-    % % Combine the compensated channels to get the final RGB image
-    % result = cat(3, compensatedR, compensatedG, compensatedB);
+    % 
+    % %Adjust the tone and intensity for each channel using imadjust
+    % adjustedRedChannel = imadjust(redChannel);
+    % adjustedGreenChannel = imadjust(greenChannel);
+    % adjustedBlueChannel = imadjust(blueChannel);
+    % 
+    % %Combine the adjusted channels to get the final adjusted image
+    % result = cat(3, adjustedRedChannel, adjustedGreenChannel, adjustedBlueChannel);
 
-    % Separate the image into three channels (R, G, B)
-    redChannel = originalRGB(:, :, 1);
-    greenChannel = originalRGB(:, :, 2);
-    blueChannel = originalRGB(:, :, 3);
+    %% grayworld
+    % Split the image to r, g and b channels
+    r = originalRGB(:,:,1);
+    g = originalRGB(:,:,2);
+    b = originalRGB(:,:,3);
     
-    % Adjust the tone and intensity for each channel using imadjust
-    adjustedRedChannel = imadjust(redChannel);
-    adjustedGreenChannel = imadjust(greenChannel);
-    adjustedBlueChannel = imadjust(blueChannel);
+    % Compute mean:
+    r_avg = mean(r);
+    g_avg = mean(g);
+    b_avg = mean(b);
     
-    % Combine the adjusted channels to get the final adjusted image
-    result = cat(3, adjustedRedChannel, adjustedGreenChannel, adjustedBlueChannel);
+    % Compute the gain for the red and green channel:
+    alpha = g_avg/r_avg;
+    beta = g_avg/b_avg;
+    
+    % Compute color corrected image
+    r_sensor = alpha*r;
+    g_sensor = g;
+    b_sensor = beta*b;
+    
+    result = cat(3, r_sensor,g_sensor,b_sensor);
+    result=result*0.9;
 
 end
-
